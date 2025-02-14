b4439c1dd4f96a9004ceeb584842016cd227b210
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-02-13 23:44:26 -0300

    Agrega configuración del contexto de base de datos para la entidad Client

--- Ferreteria.Infrastructure/AppDbContext.cs
+++ Ferreteria.Infrastructure/AppDbContext.cs
@@ -18,6 +18,7 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
     }
 
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Client> Clients { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -29,5 +30,18 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
             entity.Property(e => e.Name).HasColumnName("name").HasColumnType("TEXT").HasMaxLength(100).IsRequired();
             entity.Property(e => e.Description).HasColumnName("description").HasColumnType("TEXT").HasMaxLength(255).IsRequired();
         });
+
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.ToTable("client","ferreteria");
+            entity.HasKey(e => e.IdClient);
+            entity.Property(e => e.FirstName).HasColumnName("first_name").HasColumnType("TEXT").HasMaxLength(50).IsRequired();
+            entity.Property(e => e.LastName).HasColumnName("last_name").HasColumnType("TEXT").HasMaxLength(50);
+            entity.Property(e => e.Phone).HasColumnName("phone").HasColumnType("NUMERIC");
+            entity.Property(e => e.Email).HasColumnName("email").HasColumnType("TEXT").HasMaxLength(100);
+            entity.Property(e => e.Address).HasColumnName("address").HasColumnType("TEXT").HasMaxLength(255);
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("NUMERIC").HasDefaultValue("DATETIME('now')");
+        });
+
     }
 }
