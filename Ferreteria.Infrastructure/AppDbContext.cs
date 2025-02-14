536195aac0c5cfa513a073f7ca7d793f645392ad
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-02-13 23:43:50 -0300

    Agrega configuración del contexto de base de datos para la entidad Category

--- Ferreteria.Infrastructure/AppDbContext.cs
+++ Ferreteria.Infrastructure/AppDbContext.cs
@@ -1,5 +1,7 @@
 using System;
 
+using Ferreteria.Domain.Entities;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -13,5 +15,19 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
         {
             optionsBuilder.UseSqlite(configuration.GetConnectionString("DefaultConnectionString"));
         }
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.ToTable("category", "ferreteria");
+            entity.HasKey(e => e.IdCategory);
+            entity.Property(e => e.IdCategory).ValueGeneratedOnAdd().IsRequired();
+            entity.Property(e => e.Name).HasColumnName("name").HasColumnType("TEXT").HasMaxLength(100).IsRequired();
+            entity.Property(e => e.Description).HasColumnName("description").HasColumnType("TEXT").HasMaxLength(255).IsRequired();
+        });
     }
 }
