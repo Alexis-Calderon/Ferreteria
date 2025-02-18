9cdbedce8bc5460e2812b96eafa5e0a890042082
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-02-17 21:40:34 -0300

    Agrega la entidad Product al contexto de base de datos y define sus propiedades

--- Ferreteria.Infrastructure/AppDbContext.cs
+++ Ferreteria.Infrastructure/AppDbContext.cs
@@ -22,6 +22,7 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
     public DbSet<Employee> Employees { get; set; }
     public DbSet<Inventory> Inventories { get; set; }
     public DbSet<InventoryMovement> InventoryMovements { get; set; }
+    public DbSet<Product> Products { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -77,6 +78,21 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
             entity.Property(e =>e.Quantity).HasColumnName("quantity").HasColumnType("INTEGER").IsRequired();
             entity.Property(e => e.MovementDate).HasColumnName("movement_date").HasColumnType("TEXT").IsRequired();
             entity.Property(e =>e.Reference).HasColumnName("reference").HasColumnType("TEXT").HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Product>(entity => {
+            entity.ToTable("product", "ferreteria");
+            entity.HasKey(e => e.IdProduct);
+            entity.Property(e => e.IdProduct).HasColumnName("id_product").ValueGeneratedOnAdd().IsRequired();
+            entity.Property(e => e.Name).HasColumnName("name").HasColumnType("TEXT").HasMaxLength(100).IsRequired();
+            entity.Property(e => e.Description).HasColumnName("description").HasColumnType("TEXT").HasMaxLength(255);
+            entity.Property(e => e.Category).HasColumnName("category").HasColumnType("TEXT").HasMaxLength(100);
+            entity.Property(e => e.Brand).HasColumnName("brand").HasColumnType("TEXT").HasMaxLength(20);
+            entity.Property(e => e.Price).HasColumnName("price").HasColumnType("NUMERIC").IsRequired();
+            entity.Property(e => e.Stock).HasColumnName("stock").HasColumnType("INTEGER").IsRequired();
+            entity.Property(e => e.MeasurementUnit).HasColumnName("measurement_unit").HasColumnType("TEXT").HasMaxLength(10);
+            entity.Property(e => e.IdSupplier).HasColumnName("id_supplier").HasColumnType("INTEGER");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
         });
     }
 }
