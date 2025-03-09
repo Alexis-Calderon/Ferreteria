71bda3c7d825a4420090e993b77f2432208976fb
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-03-08 21:47:00 -0300

    Actualiza el DbContext con los nuevos cambios de las entidades

--- Ferreteria.Infrastructure/Context/ApplicationDbContext.cs
+++ Ferreteria.Infrastructure/Context/ApplicationDbContext.cs
@@ -1,20 +1,17 @@
-using System;
-
 using Ferreteria.Core.Entities;
-
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace Ferreteria.Infrastructure.Context;
 
-public class AppDbContext(DbContextOptions options, IConfiguration configuration) : DbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseSqlite(configuration.GetConnectionString("DefaultConnectionString"));
+            optionsBuilder.UseSqlite("Data Source=Ferreteria.db");
         }
+        base.OnConfiguring(optionsBuilder);
     }
 
     public DbSet<Category> Categories { get; set; }
@@ -33,16 +30,22 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
     {
         modelBuilder.Entity<Category>(entity =>
         {
-            entity.ToTable("category", "ferreteria");
+            entity.ToTable("category");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdCategory);
             entity.Property(e => e.IdCategory).HasColumnName("id_category").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.Name).HasColumnName("name").HasColumnType("TEXT").HasMaxLength(100).IsRequired();
             entity.Property(e => e.Description).HasColumnName("description").HasColumnType("TEXT").HasMaxLength(255).IsRequired();
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
         });
 
         modelBuilder.Entity<Client>(entity =>
         {
-            entity.ToTable("client", "ferreteria");
+            entity.ToTable("client");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdClient);
             entity.Property(e => e.IdClient).HasColumnName("id_client").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.FirstName).HasColumnName("first_name").HasColumnType("TEXT").HasMaxLength(50).IsRequired();
@@ -50,32 +53,47 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
             entity.Property(e => e.Phone).HasColumnName("phone").HasColumnType("NUMERIC");
             entity.Property(e => e.Email).HasColumnName("email").HasColumnType("TEXT").HasMaxLength(100);
             entity.Property(e => e.Address).HasColumnName("address").HasColumnType("TEXT").HasMaxLength(255);
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
             entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
         });
 
         modelBuilder.Entity<Employee>(entity =>
         {
-            entity.ToTable("employee", "ferreteria");
+            entity.ToTable("employee");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdEmployee);
             entity.Property(e => e.IdEmployee).HasColumnName("id_employee").ValueGeneratedOnAdd().IsRequired();
-            entity.Property(e => e.FirstName).HasColumnName("first_name").HasColumnType("TEXT").HasMaxLength(30).IsRequired();
-            entity.Property(e => e.LastName).HasColumnName("last_name").HasColumnType("TEXT").HasMaxLength(30);
+            entity.Property(e => e.IdUser).HasColumnName("id_user").HasColumnType("INTEGER").IsRequired();
             entity.Property(e => e.JobPosition).HasColumnName("job_position").HasColumnType("TEXT").HasMaxLength(20);
             entity.Property(e => e.Phone).HasColumnName("phone").HasColumnType("TEXT").HasMaxLength(15);
-            entity.Property(e => e.Email).HasColumnName("email").HasColumnType("TEXT").HasMaxLength(100);
-            entity.Property(e => e.HiringDate).HasColumnName("hiring_date").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.HiringDate).HasColumnName("hiring_date").HasColumnType("TEXT");
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
+
+            entity.HasOne(d => d.IdUserNavigation).WithOne(p => p.IdEmployeeNavigation)
+                .HasForeignKey<Employee>(d => d.IdUser)
+                .OnDelete(DeleteBehavior.NoAction)
+                .HasConstraintName("FK_employee_user");
         });
 
         modelBuilder.Entity<Inventory>(entity =>
         {
-            entity.ToTable("inventory", "ferreteria");
+            entity.ToTable("inventory");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdInventory);
             entity.Property(e => e.IdInventory).HasColumnName("id_inventory").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.IdProduct).HasColumnName("id_product").HasColumnType("INTEGER").IsRequired();
-            entity.Property(e => e.CurrentStock).HasColumnName("current_stock").HasColumnName("INTEGER").IsRequired();
-            entity.Property(e => e.UpdatedAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CurrentStock).HasColumnName("current_stock").HasColumnType("INTEGER").IsRequired();
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
 
-            entity.HasOne(d => d.ProductNavigation).WithMany(p => p.Inventories)
+            entity.HasOne(d => d.IdProductNavigation).WithMany(p => p.Inventories)
                 .HasForeignKey(d => d.IdProduct)
                 .OnDelete(DeleteBehavior.NoAction)
                 .HasConstraintName("FK_inventory_product");
@@ -83,7 +101,8 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
 
         modelBuilder.Entity<InventoryMovement>(entity =>
         {
-            entity.ToTable("inventory_movement", "ferreteria");
+            entity.ToTable("inventory_movement");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdInventoryMovement);
             entity.Property(e => e.IdInventoryMovement).HasColumnName("id_inventory_movement").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.IdProduct).HasColumnName("id_product").HasColumnType("INTEGER").IsRequired();
@@ -91,8 +110,12 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
             entity.Property(e => e.Quantity).HasColumnName("quantity").HasColumnType("INTEGER").IsRequired();
             entity.Property(e => e.MovementDate).HasColumnName("movement_date").HasColumnType("TEXT").IsRequired();
             entity.Property(e => e.Reference).HasColumnName("reference").HasColumnType("TEXT").HasMaxLength(50);
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
 
-            entity.HasOne(d => d.ProductNavigation).WithMany(p => p.InventoryMovements)
+            entity.HasOne(d => d.IdProductNavigation).WithMany(p => p.InventoryMovements)
                 .HasForeignKey(d => d.IdProduct)
                 .OnDelete(DeleteBehavior.NoAction)
                 .HasConstraintName("FK_inventory_movement_product");
@@ -100,7 +123,8 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
 
         modelBuilder.Entity<Product>(entity =>
         {
-            entity.ToTable("product", "ferreteria");
+            entity.ToTable("product");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdProduct);
             entity.Property(e => e.IdProduct).HasColumnName("id_product").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.Name).HasColumnName("name").HasColumnType("TEXT").HasMaxLength(100).IsRequired();
@@ -111,7 +135,10 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
             entity.Property(e => e.Stock).HasColumnName("stock").HasColumnType("INTEGER").IsRequired();
             entity.Property(e => e.MeasurementUnit).HasColumnName("measurement_unit").HasColumnType("TEXT").HasMaxLength(10);
             entity.Property(e => e.IdSupplier).HasColumnName("id_supplier").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
             entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
 
             entity.HasOne(d => d.IdCategoryNavigation).WithMany(p => p.Products)
                 .HasForeignKey(d => d.IdCategory)
@@ -126,13 +153,17 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
 
         modelBuilder.Entity<Purchase>(entity =>
         {
-            entity.ToTable("purchase", "ferreteria");
+            entity.ToTable("purchase");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdPurchase);
             entity.Property(e => e.IdPurchase).HasColumnName("id_purchase").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.IdSupplier).HasColumnName("id_supplier").HasColumnType("INTEGER");
             entity.Property(e => e.IdEmployee).HasColumnName("id_employee").HasColumnType("INTEGER");
             entity.Property(e => e.Total).HasColumnName("total").HasColumnType("NUMERIC");
-            entity.Property(e => e.PurchaseDate).HasColumnName("purchase_date").HasColumnType("TEXT").IsRequired();
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
 
             entity.HasOne(d => d.IdEmployeeNavigation).WithMany(p => p.Purchases)
                 .HasForeignKey(d => d.IdEmployee)
@@ -147,7 +178,8 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
 
         modelBuilder.Entity<PurchaseProduct>(entity =>
         {
-            entity.ToTable("purchase_product", "ferreteria");
+            entity.ToTable("purchase_product");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdPurchaseProduct);
             entity.Property(e => e.IdPurchaseProduct).HasColumnName("id_purchase_product").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.IdPurchase).HasColumnName("id_purchase").HasColumnType("INTEGER").IsRequired();
@@ -155,6 +187,10 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
             entity.Property(e => e.Quantity).HasColumnName("quantity").HasColumnType("INTEGER").IsRequired();
             entity.Property(e => e.UnitPrice).HasColumnName("unit_price").HasColumnType("NUMERIC").IsRequired();
             entity.Property(e => e.Subtotal).HasColumnName("subtotal").HasColumnType("NUMERIC").IsRequired();
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
 
             entity.HasOne(d => d.IdProductNavigation).WithMany(p => p.PurchaseProducts)
                 .HasForeignKey(d => d.IdProduct)
@@ -169,13 +205,17 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
 
         modelBuilder.Entity<Sale>(entity =>
         {
-            entity.ToTable("sale", "ferreteria");
+            entity.ToTable("sale");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdSale);
             entity.Property(e => e.IdSale).HasColumnName("id_sale").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.IdClient).HasColumnName("id_client").HasColumnType("INTEGER");
             entity.Property(e => e.IdEmployee).HasColumnName("id_employee").HasColumnType("INTEGER");
             entity.Property(e => e.Total).HasColumnName("total").HasColumnType("NUMERIC").IsRequired();
-            entity.Property(e => e.SaleDate).HasColumnName("sale_date").HasColumnType("TEXT").IsRequired();
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
 
             entity.HasOne(d => d.IdClientNavigation).WithMany(p => p.Sales)
                 .HasForeignKey(d => d.IdClient)
@@ -190,7 +230,8 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
 
         modelBuilder.Entity<SaleProduct>(entity =>
         {
-            entity.ToTable("sale_product", "ferreteria");
+            entity.ToTable("sale_product");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdSaleProduct);
             entity.Property(e => e.IdSaleProduct).HasColumnName("id_sale_product").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.IdSale).HasColumnName("id_sale").HasColumnType("INTEGER").IsRequired();
@@ -198,6 +239,10 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
             entity.Property(e => e.Quantity).HasColumnName("quantity").HasColumnType("INTEGER").IsRequired();
             entity.Property(e => e.UnitPrice).HasColumnName("unit_price").HasColumnType("NUMERIC").IsRequired();
             entity.Property(e => e.Subtotal).HasColumnName("subtotal").HasColumnType("NUMERIC").IsRequired();
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
 
             entity.HasOne(d => d.IdProductNavigation).WithMany(p => p.SaleProducts)
                 .HasForeignKey(d => d.IdProduct)
@@ -212,13 +257,34 @@ public class AppDbContext(DbContextOptions options, IConfiguration configuration
 
         modelBuilder.Entity<Supplier>(entity =>
         {
-            entity.ToTable("supplier", "ferreteria");
+            entity.ToTable("supplier");
+            entity.HasQueryFilter(e => !e.IsDeleted);
             entity.HasKey(e => e.IdSupplier);
             entity.Property(e => e.IdSupplier).HasColumnName("id_supplier").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.Name).HasColumnName("name").HasColumnType("TEXT").HasMaxLength(100).IsRequired();
             entity.Property(e => e.Phone).HasColumnName("phone").HasColumnType("TEXT").HasMaxLength(15);
             entity.Property(e => e.Email).HasColumnName("email").HasColumnType("TEXT").HasMaxLength(100);
             entity.Property(e => e.Address).HasColumnName("address").HasColumnType("TEXT").HasMaxLength(255);
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("user");
+            entity.HasQueryFilter(e => !e.IsDeleted);
+            entity.HasKey(e => e.IdUser);
+            entity.Property(e => e.IdUser).HasColumnName("id_user").ValueGeneratedOnAdd().IsRequired();
+            entity.Property(e => e.FirstName).HasColumnName("first_name").HasColumnType("TEXT").HasMaxLength(50).IsRequired();
+            entity.Property(e => e.LastName).HasColumnName("last_name").HasColumnType("TEXT").HasMaxLength(50).IsRequired();
+            entity.Property(e => e.Password).HasColumnName("password").HasColumnType("TEXT").HasMaxLength(255).IsRequired();
+            entity.Property(e => e.Email).HasColumnName("email").HasColumnType("TEXT").HasMaxLength(100).IsRequired();
+            entity.Property(e => e.UpdateBy).HasColumnName("update_by").HasColumnType("INTEGER");
+            entity.Property(e => e.UpdateAt).HasColumnName("update_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.CreatedAt).HasColumnName("create_at").HasColumnType("TEXT").HasDefaultValueSql("DATETIME('now')");
+            entity.Property(e => e.IsDeleted).HasColumnName("is_deleted").HasColumnType("INTEGER").HasDefaultValueSql("0");
         });
     }
 }
