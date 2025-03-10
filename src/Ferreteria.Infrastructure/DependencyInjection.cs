6d949ccc8791dc753639127151d52f122cbc1ae1
Alexis José Calderón Díaz <alexisjosecalderondiaz@gmail.com>
2025-03-09 23:21:56 -0300

    Actualiza la inyección de dependencias para configurar el contexto de base de datos y registrar el repositorio de productos

--- src/Ferreteria.Infrastructure/DependencyInjection.cs
+++ src/Ferreteria.Infrastructure/DependencyInjection.cs
@@ -1,15 +1,20 @@
+using Ferreteria.Domain.Interfaces;
 using Ferreteria.Infrastructure.Context;
+using Ferreteria.Infrastructure.Repositories;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ferreteria.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<ApplicationDbContext>();
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString("FerreteriaDb")));
+        services.AddScoped<IProductRepository, ProductRepository>();
 
         return services;
     }
